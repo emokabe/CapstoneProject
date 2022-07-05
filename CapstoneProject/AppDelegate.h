710f9b7fd6f72a724b8168e5f01318ab41b5f025
emokabe <emilyokb@stanford.edu>
2022-07-05 16:00:29 -0700

    Initial Commit

---
+++ CapstoneProject/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  CapstoneProject
+//
+//  Created by Emily Ito Okabe on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ CapstoneProject/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  CapstoneProject
+//
+//  Created by Emily Ito Okabe on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
