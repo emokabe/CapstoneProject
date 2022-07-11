6b033408d6aa983fcb07b56554a494f0b788a7b5
emokabe <emilyokb@stanford.edu>
2022-07-10 21:34:53 -0700

    Added compose post view

---
+++ CapstoneProject/View Controllers/ComposeViewController.m
@@ -0,0 +1,41 @@
+//
+//  ComposeViewController.m
+//  CapstoneProject
+//
+//  Created by Emily Ito Okabe on 7/10/22.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *postText;
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didTapCancel:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+- (IBAction)didTapPost:(id)sender {
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
