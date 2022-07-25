79911c347b8be3427eb7e6fb92507f7a71c42076
emokabe <emily.okabe@gmail.com>
2022-07-25 15:01:40 -0700

    Added API call for posting comment on Facebook

--- CapstoneProject/View Controllers/AnsweringViewController.m
+++ CapstoneProject/View Controllers/AnsweringViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "AnsweringViewController.h"
+#import "FBSDKCoreKit/FBSDKCoreKit.h"
 
 @interface AnsweringViewController ()
 
@@ -26,6 +27,39 @@
 
 - (IBAction)didTapCancel:(id)sender {
     [self dismissViewControllerAnimated:true completion:nil];
+}
+
+- (IBAction)didTapRespond:(id)sender {
+    [self composeAnswer];
+}
+
+
+- (void)composeAnswer {
+    NSString *messageWithDelimiters = [NSString stringWithFormat:@"%@%@%@", self.answerText.text, @"/0\n\n", self.postToAnswerInfo.post_id];
+    FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc]
+                                  initWithGraphPath:@"/425184976239857/feed"
+                                  parameters:@{ @"message": messageWithDelimiters}
+                                  HTTPMethod:@"POST"];
+    
+    [request startWithCompletion:^(id<FBSDKGraphRequestConnecting>  _Nullable connection, id  _Nullable result, NSError * _Nullable error) {
+        if (!error) {
+            NSLog(@"Success!");
+            [self dismissViewControllerAnimated:YES completion:nil];
+            /*
+            [self getPostWithID:result[@"id"] completion:^(Post *post, NSError *err) {
+                if (err) {
+                    NSLog(@"Error getting post: %@", err.localizedDescription);
+                } else {
+                    NSLog(@"Success!");
+                    [self dismissViewControllerAnimated:YES completion:nil];
+                    //[self.delegate didPost:post];
+                }
+            }];
+             */
+        } else {
+            NSLog(@"Error posting to feed: %@", error.localizedDescription);
+        }
+    }];
 }
 
 
